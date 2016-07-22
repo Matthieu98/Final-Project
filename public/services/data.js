6b06619752175d4f94be7b850b0cde2a185f96f0
Matthieu Tapolsky <matthieutapolsky@gmail.com>
2016-07-22 01:15:09 -0400

    Delete data.js

--- public/services/data.js
+++
@@ -1,8 +0,0 @@
-// 'use strict';
-//
-// angular.module('Chess_Timer_App')
-//   .service('dataService', function($http){
-//     this.getTurns = function(cb){
-//       $http.get('/api/turns').then(cb);
-//     };
-//   })
